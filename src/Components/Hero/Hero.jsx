de9941d216e437ddb3306bc688c1ba1bad10ec76
sajib689 <jibonraja11@gmail.com>
2024-03-23 17:20:07 +0600

    add img in bottom

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
-
 const Hero = () => {
   return (
-    <div className="hero  bg-base-200">
+    <div className="hero  -mt-20">
       <div className="hero-content text-center">
         <div className="max-w-lg">
           <h1 className="text-5xl font-bold">
@@ -22,14 +21,22 @@ const Hero = () => {
               className="relative inline-block px-4 py-2 font-medium group"
             >
               <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-              <span className="absolute inset-0 w-full h-full bg-white border-2 border-primary group-hover:bg-primary"></span>
-              <span className="relative text-primary group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+              <span className="relative text-black group-hover:text-white">
                 Read Blogs
               </span>
             </Link>
-            <Link to="/bookmarks">
-              <button className="btn btn-primary">Bookmarks</button>
+            <Link
+              to="/bookmarks"
+              className="relative inline-block px-4 py-2 font-medium group"
+            >
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+              <span className="relative text-black group-hover:text-white">
+              Bookmarks
+              </span>
             </Link>
+            
           </div>
         </div>
       </div>
--- src/Pages/MainHome/MainHome.jsx
+++ src/Pages/MainHome/MainHome.jsx
@@ -1,9 +1,10 @@
 import Hero from "../../Components/Hero/Hero";
-
+import wave from '../../assets/wave.svg'
 const MainHome = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
+    <div className="flex flex-col relative items-center justify-center min-h-[calc(100vh-116px)]">
         <Hero/>
+        <img className="absolute bottom-0 left-0 w-[100%]" src={wave} alt="" />
     </div>
   );
 };
